f2c64be00ab14349f456dc9838260da283190018
Walter Vos <walter.vos@ns.nl>
2024-03-27 14:04:27 +0100

    refactor

--- tdd_gol/__init__.py
+++ tdd_gol/__init__.py
@@ -1,36 +1,25 @@
 class Matrix:
     def __init__(self, cells) -> None:
         self.cells = cells
+        self._neigbour_offset = [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ]
 
     def neighbours_for(self, row, column):
-        return (
-            [
-                self._cell_at(row - 1, column - 1),
-                self._cell_at(row - 1, column),
-                self._cell_at(row - 1, column + 1),
-                self._cell_at(row, column - 1),
-                self._cell_at(row, column + 1),
-                self._cell_at(row + 1, column - 1),
-                self._cell_at(row + 1, column),
-                self._cell_at(row + 1, column + 1),
-            ]
-            if column == 1
-            else [
-                self._cell_at(row - 1, column),
-                self._cell_at(row - 1, column + 1),
-                self._cell_at(row, column + 1),
-                self._cell_at(row + 1, column),
-                self._cell_at(row + 1, column + 1),
-            ]
-            if column == 0
-            else [
-                self._cell_at(row - 1, column - 1),
-                self._cell_at(row - 1, column),
-                self._cell_at(row, column - 1),
-                self._cell_at(row + 1, column - 1),
-                self._cell_at(row + 1, column),
-            ]
-        )
+        result = []
+        for (row_offset, column_offset) in self._neigbour_offset:
+            if (row + row_offset) >= 0 and (row + row_offset) < len(self.cells):
+                if (column + column_offset) >= 0 and (column + column_offset) < len(self.cells):
+                    result.append(self._cell_at(row+row_offset, column+column_offset))
+        return result
+
 
     def _cell_at(self, row, column):
         return self.cells[row][column]
