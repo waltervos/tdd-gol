bd23b73410553656e5c87461843cd88a4b07e4a1
Walter Vos <walter.vos@ns.nl>
2024-03-27 08:47:21 +0100

    Initial commit

---
+++ tests/test_gol.py
@@ -0,0 +1,9 @@
+# Rules of the game:
+# Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+# Any live cell with two or three live neighbors lives on to the next generation.
+# Any live cell with more than three live neighbors dies, as if by overpopulation.
+# Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+# Finding neighbours:
+# (at least) One position away from every edge, a cell has eight neighbours
+# At the edges a cell has fewer neighbours (the matrix isn't a globe): 3 or 5
