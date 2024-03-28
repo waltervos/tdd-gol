8b839136bcdf2b189ac8610c8269e065f210e531
Walter Vos <walter.vos@ns.nl>
2024-03-28 19:53:36 +0100

    red

--- tests/test_gol.py
+++ tests/test_gol.py
@@ -113,12 +113,12 @@ class DescribeRunningTheGame:
                 [a_live_cell(), a_live_cell()]
             ]
         
-    # def it_maintains_a_blinker(self):
-    #     game = Game(width=3, height=3, life_at=[(0,1), (1,1), (2,1)])
-    #     game.next_generation()
-    #     game_state = game.get_state()
-    #     assert game_state['board'] == [
-    #         [a_dead_cell(), a_dead_cell(), a_dead_cell()],
-    #         [a_live_cell(), a_live_cell(), a_live_cell()],
-    #         [a_dead_cell(), a_dead_cell(), a_dead_cell()],
-    #     ]
+    def it_maintains_a_blinker(self):
+        game = Game(width=3, height=3, life_at=[(0,1), (1,1), (2,1)])
+        game.next_generation()
+        game_state = game.get_state()
+        assert game_state['board'] == [
+            [a_dead_cell(), a_dead_cell(), a_dead_cell()],
+            [a_live_cell(), a_live_cell(), a_live_cell()],
+            [a_dead_cell(), a_dead_cell(), a_dead_cell()],
+        ]
