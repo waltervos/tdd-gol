adf15359eae9919c09dada0877a9f555e8bcbcbc
Walter Vos <walter.vos@ns.nl>
2024-03-29 23:14:08 +0100

    prefactor

--- tdd_gol/__init__.py
+++ tdd_gol/__init__.py
@@ -20,7 +20,9 @@ class Matrix:
         for row_offset, column_offset in self._neigbour_offset:
             neighbour_column_index = column + column_offset
             neighbour_row_index = row + row_offset
-            if 0 <= neighbour_column_index < len(self.cells) and 0 <= neighbour_row_index < len(self.cells[0]):
+            if 0 <= neighbour_column_index < len(
+                self.cells[0]
+            ) and 0 <= neighbour_row_index < len(self.cells):
                 result.append(
                     self._cell_at(neighbour_row_index, neighbour_column_index)
                 )
@@ -31,7 +33,7 @@ class Matrix:
 
     def __eq__(self, other: "Matrix") -> bool:
         return self.cells == other.cells
-    
+
     def __repr__(self) -> str:
         return str([cell for cell in [row for row in self.cells]])
 
@@ -54,7 +56,7 @@ class Cell:
 
     def __eq__(self, other: "Cell") -> bool:
         return self.is_alive() == other.is_alive()
-    
+
     def __repr__(self) -> str:
         return "Alive" if self.is_alive() else "Dead"
 
@@ -66,13 +68,27 @@ def a_live_cell() -> Cell:
 def a_dead_cell() -> Cell:
     return Cell(alive=False)
 
+
 class Game:
     def __init__(self, board: Matrix) -> None:
         self._board = board
 
     def next(self):
-        pass
+        new_cells = []
+        for row_index, row in enumerate(self._board.cells):
+            new_cells.append([])
+            for cell_index, cell in enumerate(row):
+                neighbours = self._board.neighbours_for(row_index, cell_index)
+                new_cell: Cell = cell.next_generation(neighbours)
+                new_cells[row_index].append(new_cell)
+
+        self._board = Matrix(new_cells)
 
     @property
     def board(self):
-        return Matrix([[a_dead_cell()] for _ in range(0, len(self._board.cells))])
+        return Matrix(
+            [
+                [a_dead_cell() for _ in range(0, len(self._board.cells[0]))]
+                for _ in range(0, len(self._board.cells))
+            ]
+        )
